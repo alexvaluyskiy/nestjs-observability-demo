bbdafd3aa1c5dff0cff07de0535782f5c2737406
alexvaluyskiy <alex.valuiskyi@gmail.com>
2022-02-08 18:36:21 +0200

    add tracing

--- src/telemetry.ts
+++ src/telemetry.ts
@@ -1,13 +1,40 @@
 import { OpenTelemetryModule } from "nestjs-otel";
 import { NodeSDK } from '@opentelemetry/sdk-node';
 import { PrometheusExporter } from '@opentelemetry/exporter-prometheus';
+import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-base';
+import { JaegerExporter } from '@opentelemetry/exporter-jaeger';
+import { JaegerPropagator } from '@opentelemetry/propagator-jaeger';
+import { AsyncLocalStorageContextManager } from '@opentelemetry/context-async-hooks';
+import { Resource } from '@opentelemetry/resources';
+import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
+
+// instrumentation
+import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
+import { MySQL2Instrumentation } from '@opentelemetry/instrumentation-mysql2';
+
+const jaegerExporter = new JaegerExporter({
+  tags: [],
+  endpoint: 'http://localhost:14268/api/traces', // TODO: take it from the config
+  maxPacketSize: 65000
+});
 
 export const otelSDK = new NodeSDK({
   metricExporter: new PrometheusExporter({
     port: 8081,
   }),
   metricInterval: 1000,
-  instrumentations: [],
+  resource: new Resource({
+    [SemanticResourceAttributes.SERVICE_NAME]: 'nestjs-observability-demo',
+  }),
+  contextManager: new AsyncLocalStorageContextManager(),
+  spanProcessor: new BatchSpanProcessor(jaegerExporter),
+  textMapPropagator: new JaegerPropagator(), 
+  instrumentations: [
+    new HttpInstrumentation({
+      ignoreIncomingPaths: ['/metrics', '/favicon.ico']
+    }),
+    new MySQL2Instrumentation()
+  ],
 });
 
 export const OpenTelemetryModuleConfig = OpenTelemetryModule.forRoot({
